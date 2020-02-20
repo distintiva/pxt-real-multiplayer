12548924f79b862dd4a4c7970254e660bf5bba5b
Distintiva Solutions <57623220+distintiva@users.noreply.github.com>
2020-02-20 20:22:19 +0100

    Update common.ts, test.ts

--- common.ts
+++ common.ts
@@ -27,9 +27,9 @@ namespace multiplayer {
     export function getImageId(im: Image): number {
         let imcrc = 0;
         for (let f = 0; f < im.height; f++) {
-            for (let c = 0; c < im.width; c++) {
+            for (let c = f%2; c < im.width; c+=2) {
                 let px = im.getPixel(f, c);
-                imcrc +=px * ( c + (c * f) );
+                imcrc +=px * ( c + f ) +1;
             }
         }
         return imcrc;
--- test.ts
+++ test.ts
@@ -1,28 +1,67 @@
 namespace SpriteKind {
     export const PJ1 = SpriteKind.create()
+    export const PJ2 = SpriteKind.create()
 }
+
+ 
+
+
+
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+
     rand = sprites.create(img`
-        . . . . f f f f f . . . . . . .
-        . . . f e e e e e f . . . . . .
-        . . f d d d d e e e f . . . . .
-        . c d f d d f d e e f f . . . .
-        . c d f d d f d e e d d f . . .
-        c d e e d d d d e e b d c . . .
-        c d d d d c d d e e b d c . f f
-        c c c c c d d d e e f c . f e f
-        . f d d d d d e e f f . . f e f
-        . . f f f f f e e e e f . f e f
-        . . . . f e e e e e e e f f e f
-        . . . f e f f e f e e e e f f .
-        . . . f e f f e f e e e e f . .
-        . . . f d b f d b f f e f . . .
-        . . . f d d c d d b b d f . . .
-        . . . . f f f f f f f f f . . .
-    `, SpriteKind.Enemy)
+        f f f f f f f f f f f f f f f f
+        f f f f f f f f f f f f f f f f
+        f f f d d d d f f f f f f f f f
+        f c d f d d f d f f f f f f f f
+        f c d f d d f d f f d d f f f f
+        c d f f d d d d f f b d c f f f
+        c d d d d c d d f f b d c f f f
+        c c c c c d d d f f f c f f f f
+        f f d d d d d f f f f f f f f f
+        f f f f f f f f f f f f f f f f
+        f f f f f f f f f f f f f f f f
+        f f f f f f f f f f f f f f f f
+        f f f f f f f f f f f f f f f f
+        f f f f d b f d b f f f f f f f
+        f f f f d d c d d b b d f f f f
+        f f f f f f f f f f f f f f f f
+    `, SpriteKind.PJ2)
     rand.setPosition(Math.randomRange(0, 160), Math.randomRange(0, 120))
     rand.setVelocity(20, 0)
-    multiplayer.syncThisSprite(rand)
+
+    
+
+   
+ 
+   
+
+
+    /*for (const k in game.currentScene().spritesByKind() ){
+        console.log("K:" + k);
+    }*/
+   
+    multiplayer.syncThisSprite(rand) 
+    //- le meto un data (si no lo tiene) = al numero del jugador que lo crea y un ID
+    //- si no tiene data, le asigno un ID
+    //- si es player 2 inicializo el ID de sprites en 1000
+
+   /* for( let k in SpriteKind){
+        console.log("Kind " + k);
+    }*/
+   
+
+})
+
+sprites.onCreated(0, function (sprite: Sprite) {
+    console.log("ID2:" + sprite.id);
+    sprite.id=10;
+    console.log("ID3:" + sprite.id);
+
+    setInterval(function () {
+        //- si tiene Data es del otro jugador
+        console.log("parametros");
+    }, 50)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     console.log(SpriteKind.PJ1)
@@ -49,6 +88,8 @@ multiplayer.onConnected(function () {
         . . f f 1 d 1 d 1 d f f . . . .
         . . . . f f b b f f . . . . . .
     `, SpriteKind.PJ1)
+
+    console.log("ID:" + mySprite.id);
 })
 let mySprite: Sprite = null
 let rand: Sprite = null
@@ -88,4 +129,4 @@ multiplayer.sharedImgs([img`
     . . f f 1 d 1 d 1 d f f . . . .
     . . . . f f b b f f . . . . . .
 `])
-multiplayer.waitForConnection(true)
+multiplayer.waitForConnection(false)
